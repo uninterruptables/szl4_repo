092ce248ed5e6d82ccb4ff8307ac4d534ad1fc48
RedCloud <icloudred@gmail.com>
2014-03-23 18:18:44 +0100

    MordorQuest: resolving errors.

--- szl4_project_1/src/team/uninter/mordorq/gamespace/Casted.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/Casted.java
@@ -2,18 +2,26 @@ package team.uninter.mordorq.gamespace;
 
 import java.awt.Color;
 
+@SuppressWarnings("serial")
 abstract public class Casted extends GameObject {
 
 	protected int manaCost;
 	
-	public Casted(int x, int y) {
+	protected Casted() {
+		super();
+		// TODO remove sysout
+		System.out.println("Casted.Casted() called");
+		System.out.println("Casted.Casted() returned");
+	}
+	
+	protected Casted(int x, int y) {
 		super(x, y);
 		// TODO remove sysout
 		System.out.println("Casted.Casted(x,y) called");
 		System.out.println("Casted.Casted(x,y) returned");
 	}
 	
-	public Casted(int x, int y, int manaCost) {
+	protected Casted(int x, int y, int manaCost) {
 		super(x, y);
 		// TODO remove sysout
 		System.out.println("Casted.Casted(x,y,manaCost) called");
@@ -21,14 +29,7 @@ abstract public class Casted extends GameObject {
 		System.out.println("Casted.Casted(x,y,manaCost) returned");
 	}
 	
-	public Casted(int manaCost) {
-		// TODO remove sysout
-		System.out.println("Casted.Casted(manaCost) called");
-		this.manaCost = manaCost;
-		System.out.println("Casted.Casted(manaCost) returned");
-	}
-	
-	abstract public void castOn(GameObject object);
+	abstract public void castOn(TerrainGrid grid);
 	
 	public int getCost(){
 		// TODO remove sysout
--- szl4_project_1/src/team/uninter/mordorq/gamespace/InjectionTarget.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/InjectionTarget.java
@@ -5,28 +5,21 @@ abstract public class InjectionTarget extends Casted{
 
 	private int remainingRunePlace;
 	
-	public InjectionTarget(int x, int y) {
+	protected InjectionTarget() {
+		super();
+		// TODO remove sysout
+		System.out.println("InjectionTarget.InjectionTarget() called");
+		System.out.println("InjectionTarget.InjectionTarget() returned");
+	}
+	
+	protected InjectionTarget(int x, int y) {
 		super(x, y);
 		// TODO remove sysout
 		System.out.println("InjectionTarget.InjectionTarget(x,y) called");
 		System.out.println("InjectionTarget.InjectionTarget(x,y) returned");
 	}
 	
-	public InjectionTarget(int x, int y, int manaCost) {
-		super(x, y, manaCost);
-		// TODO remove sysout
-		System.out.println("InjectionTarget.InjectionTarget(x,y,manaCost) called");
-		System.out.println("InjectionTarget.InjectionTarget(x,y,manaCost) returned");
-	}
-	
-	public InjectionTarget(int manaCost) {
-		// TODO remove sysout
-		super(manaCost);
-		System.out.println("Casted.Casted(manaCost) called");
-		System.out.println("Casted.Casted(manaCost) returned");
-	}
-	
-	abstract public void injectOn(Injectable injectable);
+	abstract public void inject(Injectable injectable);
 	
 	public int getRunesRemained(){
 		// TODO remove sysout
