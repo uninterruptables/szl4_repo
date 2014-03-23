beeeda9111606f251d8cb59f64a42fa923e1f356
Celestine <Celestine@192.168.1.102>
2014-03-23 23:20:23 +0100

    Gimli and Legolas

--- szl4_project_1/src/team/uninter/mordorq/gamespace/Gimli.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/Gimli.java
@@ -5,4 +5,14 @@ import team.uninter.mordorq.skeleton.TroopStepSimulator;
 @SuppressWarnings("serial")
 public class Gimli extends Dwarf{
 
+	 private static final Gimli INSTANCE = new Gimli();
+
+	  protected Gimli(){
+		  super.health = 800;
+		  super.damage = 250;
+	  }
+
+	  public static Gimli getInstance(){
+		  return INSTANCE;
+	  }
 }
