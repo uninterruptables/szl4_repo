2efb9e3c6168521014fbca1e76f62f16bd707569
Celestine <Celestine@192.168.1.102>
2014-03-24 01:51:30 +0100

    szerk

--- szl4_project_1/src/team/uninter/mordorq/gamespace/EnemyTroop.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/EnemyTroop.java
@@ -26,56 +26,56 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 
 final public void controlIt()
 {
-		EnemyTroop.controlIt();
+		
 }
 
 public void addAll(List<StatusModifier> modifiers){}
 public void setCurrentGrid(RoadGrid)
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.setCurrentGrid(RoadGrid) called");
 	System.out.println("EnemyTroop.setCurrentGrid(RoadGrid) returned");
 }
 
 public boolean isActive()
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.isActive() called");
 	System.out.println("EnemyTroop.isActive() returned");
 }
 
 public void interactWith(EnemyTroop troop)
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.interactWith(EnemyTroop) called");
 	System.out.println("EnemyTroop.interactWith(EnemyTroop) returned");	
 }
 public abstract void interactWith(Missile missile); 
 private void add(StatusModifier sm)
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.add(StatusModifier) called");
 	System.out.println("EnemyTroop.add(StatusModifier) returned");		
 }
 public int getDamage()
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.getDamage() called");
 	System.out.println("EnemyTroop.getDamage() returned");		
 }
 public int setDamage(int damage)
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.setDamage(damage) called");
 	System.out.println("EnemyTroop.setDamage(damage) returned");	
 }
 public int getHealth()
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.getHealth() called");
 	System.out.println("EnemyTroop.getHealth() returned");	
 }
 
 public int getCooldown()
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.getCooldown() called");
 	System.out.println("EnemyTroop.getCooldown() returned");	
 }
 public int setCooldown(int cooldown)
-{
+{//TODO remove sysout
 	System.out.println("EnemyTroop.setCooldown(cooldown) called");
 	System.out.println("EnemyTroop.setCooldown(cooldown) returned");	
 }
