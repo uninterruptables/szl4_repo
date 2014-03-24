a3e68c5f449d042465415e26b334ebd5c0aa4648
Celestine <Celestine@Aurora>
2014-03-24 23:25:29 +0100

    comment

--- szl4_project_1/src/team/uninter/mordorq/gamespace/EnemyTroop.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/EnemyTroop.java
@@ -8,8 +8,17 @@ import team.uninter.mordorq.utils.PathFinder;
 
 @SuppressWarnings("serial")
 abstract public class EnemyTroop extends DamageTaker implements Controlable{
-// Class used to make a basic enemies, which will be the parent of all kind of species,enemies
-//It is set all common attributes,roles for all species, and how they will act at the scene.
+	/**
+	 * Class used to make a basic enemies, which will be the parent of all kind of species,enemies
+	 *	It is set all common attributes,roles for all species, and how they will act at the scene.
+	 **/
+	
+	/**
+	 * All attributes for enemies and for their children. How many damage they can take on barricade,
+	 * how many health they have, how fast they can move from a roadgrid to an another roadgrid.
+	 * If they fall at the storm of the battle, how many mana (reward) will go to the player.
+	 * 
+	 */
 	protected int cooldown;
 	protected int MaxCooldown;
 	protected int damage;
@@ -19,13 +28,22 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 	protected RoadGrid targetGrid;
 	ArrayList<StatusModifier> statusModifiers;
 	
+	/**
+	 * EnemyTroop no parameter
+	 */
 	public EnemyTroop() {
+		
 		super();
 		//TODO remove sysout
 		System.out.println("EnemyTroop.EnemyTroop() called");
 		System.out.println("EnemyTroop.EnemyTroop() returned");
 	} 
 	
+	/**
+	 * EnemyTroop with two parameters
+	 * @param x
+	 * @param y
+	 */
 	public EnemyTroop(int x, int y) {
 		super(x, y);
 		//TODO remove sysout
@@ -33,6 +51,13 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.EnemyTroop(x,y) returned");
 	}
 	
+	/**EnemyTroop with five parameter, attributes.
+	 * @param x
+	 * @param y
+	 * @param health
+	 * @param damage
+	 * @param reward
+	 */
 	public EnemyTroop(int x, int y, int health, int damage, int reward) {
 		super(x, y);
 		//TODO remove sysout
@@ -43,6 +68,10 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.EnemyTroop(x,y,health,damage,reward) returned");
 	}
 
+	/* This class used to EnemyTroop act at scene, at grids.
+	 * (non-Javadoc)
+	 * @see team.uninter.mordorq.gamespace.Controlable#controlIt()
+	 */
 	public final void controlIt()
 	{
 		//TODO remove sysout
@@ -63,6 +92,10 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.controlIt() returned");	
 	}
 
+	/**
+	 * THis class set the troops current grids.
+	 * @param grid
+	 */
 	public final void setCurrentGrid(RoadGrid grid)
 	{
 		//TODO remove sysout
@@ -71,6 +104,11 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.setCurrentGrid(RoadGrid) returned");
 	}
 
+	/*This class used to check if a troop have more health than 0. 
+	 * If not....the troop is not active more, means the trrop is dead.
+	 *  (non-Javadoc)
+	 * @see team.uninter.mordorq.gamespace.Controlable#isActive()
+	 */
 	public final boolean isActive()
 	{
 		//TODO remove sysout
@@ -95,6 +133,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 	
 	public abstract void interactWith(Missile missile);
 	
+	/** Troop get a modifier for their status from a magic or trap.
+	 * @param sm
+	 */
 	public final void add(StatusModifier sm)
 	{
 		//TODO remove sysout
@@ -103,6 +144,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.add(StatusModifier) returned");		
 	}
 	
+	/**Modifier all troop status.
+	 * @param sm
+	 */
 	public final void addAll(List<StatusModifier> sm)
 	{
 		//TODO remove sysout
@@ -113,6 +157,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.addAll(List<StatusModifier>) returned");		
 	}
 	
+	/** Return with the value of the troop damage
+	 * @return
+	 */
 	public final int getDamage()
 	{
 		//TODO remove sysout
@@ -121,6 +168,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		return this.damage;		
 	}
 	
+	/** This method set the value of the troop damage.
+	 * @param damage
+	 */
 	public final void setDamage(int damage)
 	{
 		//TODO remove sysout
@@ -129,6 +179,10 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		System.out.println("EnemyTroop.setDamage(damage) returned");	
 	}
 	
+	/* Return with the troop health 
+	 * (non-Javadoc)
+	 * @see team.uninter.mordorq.gamespace.DamageTaker#getHealth()
+	 */
 	public final int getHealth()
 	{
 		//TODO remove sysout
@@ -137,6 +191,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		return this.health;	
 	}
 
+	/** Return the current value of Cooldown.
+	 * @return
+	 */
 	public final int getCooldown()
 	{
 		//TODO remove sysout
@@ -145,6 +202,9 @@ abstract public class EnemyTroop extends DamageTaker implements Controlable{
 		return cooldown;	
 	}
 	
+	/** This method can set the value of the cooldown
+	 * @param cooldown
+	 */
 	public final void setCooldown(int cooldown)
 	{
 		//TODO remove sysout
