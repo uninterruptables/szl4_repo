3a36069b8c91fd8703ba2a19b62b49f388afcbb4
RedCloud <icloudred@gmail.com>
2014-03-24 23:46:50 +0100

    ...

--- szl4_project_1/src/team/uninter/mordorq/gamespace/Tower.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/Tower.java
@@ -203,7 +203,6 @@ abstract public class Tower extends InjectionTarget
 			racialDamages.put("ELF", 10);
 			racialDamages.put("HOBBIT", 10);
 			state = MissileState.WAITING;
-			deltaXY = 2;
 			System.out.println("Missile.Missile(x,y) returned");
 		}
 		
