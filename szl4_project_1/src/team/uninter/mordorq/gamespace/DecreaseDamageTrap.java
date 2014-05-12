55906c48c4f8931fc52565ea1df2577945ef009e
soma <galso5@gmail.com>
2014-05-12 20:39:51 +0200

    trap image wires

--- szl4_project_1/src/team/uninter/mordorq/gamespace/DecreaseDamageTrap.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/DecreaseDamageTrap.java
@@ -1,11 +1,23 @@
 package team.uninter.mordorq.gamespace;
 
+import java.awt.Image;
+import java.io.File;
+import java.util.HashMap;
+
+import team.uninter.mordorq.gamespace.Casted.ImageColor;
 import team.uninter.mordorq.utils.GameConstants;
 
 public class DecreaseDamageTrap extends Trap{
 	
 	//Class use to create a trap, that decreases the enemy troops damage, by applying a
 	//WeakenStatus statusmodifier
+	
+	static {
+		avalImages = new HashMap<ImageColor, Image>();
+		Casted.tryLoad(avalImages, ImageColor.RED, new File("resources/images/weak_trap_grid_16_red.jpg"));
+		Casted.tryLoad(avalImages, ImageColor.NORMAL, new File("resources/images/weak_trap_grid_16p.png"));
+	}
+	
 	public DecreaseDamageTrap() {
 		super();
 //		System.out.println("DecreaseDamageTrap() called");
--- szl4_project_1/src/team/uninter/mordorq/gamespace/PoisonTrap.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/PoisonTrap.java
@@ -1,8 +1,20 @@
 package team.uninter.mordorq.gamespace;
 
+import java.awt.Image;
+import java.io.File;
+import java.util.HashMap;
+
+import team.uninter.mordorq.gamespace.Casted.ImageColor;
+
 public class PoisonTrap extends Trap {
 	//Class use to create a trap, that decreases the enemy troops health, by applying a
 	//PoisonStatus statusmodifier
+	
+	static {
+		avalImages = new HashMap<ImageColor, Image>();
+		Casted.tryLoad(avalImages, ImageColor.RED, new File("resources/images/poison_trap_grid_2_16_red.png"));
+		Casted.tryLoad(avalImages, ImageColor.NORMAL, new File("resources/images/poison_trap_grid_2_16p.png"));
+	}
 	
 	public PoisonTrap() {
 		super();
