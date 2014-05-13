d70572a511108e4d6afaa75d08b740774918f78a
RedCloud <icloudred@gmail.com>
2014-05-13 22:03:43 +0200

    MQ

--- szl4_project_1/src/team/uninter/mordorq/gamespace/Rune.java
+++ szl4_project_1/src/team/uninter/mordorq/gamespace/Rune.java
@@ -1,11 +1,7 @@
 package team.uninter.mordorq.gamespace;
 
-import org.apache.log4j.Logger;
-
 @SuppressWarnings("serial")
 public abstract class Rune extends Casted implements Injectable {
-
-	private static final Logger logger = Logger.getLogger(Rune.class);
 
 	protected Rune() {
 		super();
@@ -17,32 +13,24 @@ public abstract class Rune extends Casted implements Injectable {
 
 	@Override
 	public boolean canCastOn(TerrainGrid grid) {
-		logger.debug("in Rune.canCastOn for " + this.toString());
 
 		InjectionTarget target = grid.getInjectionTarget();
 		if (target != null) {
 			boolean ret = target.canInject(this);
-			logger.debug("Rune.canCastOn " + grid.toString() + " returns " + ret + " for " + this.toString());
 			return ret;
 		}
 		else {
-			logger.debug("Rune.canCastOn returns false for " + this.toString() + " in [else]");
 			return false;
 		}
 	}
 
 	@Override
 	public final void castOn(TerrainGrid grid) {
-		logger.debug("in Rune.castOn " + grid.toString() + " for " + this.toString());
-
 		InjectionTarget target = grid.getInjectionTarget();
 		if (target != null) {
 			if (target.canInject(this)) {
 				target.inject(this);
-				logger.debug("grid " + grid.toString() + " injected " + this.toString());
 			}
 		}
-		logger.debug("on " + grid.toString() + " injectionTarget was null: " + (target == null));
 	}
-
 }
