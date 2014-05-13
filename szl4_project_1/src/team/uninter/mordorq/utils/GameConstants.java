9364477396fbe425cf60357aa5ce047724a7b39f
RedCloud <icloudred@gmail.com>
2014-05-13 15:58:05 +0200

    MQ: ..

--- szl4_project_1/src/team/uninter/mordorq/utils/GameConstants.java
+++ szl4_project_1/src/team/uninter/mordorq/utils/GameConstants.java
@@ -1,155 +1,150 @@
 package team.uninter.mordorq.utils;
 
 public class GameConstants {
-	
-	//game constants
+
+	// game constants
 	public static boolean RANDOMNESS = true;
-	
+
 	public static int STATUS_MODIFIER_ID = 1;
 	public static int PLACEABLE_RUNES = 1;
-	
+
 	public static int FPS = 30;
 	public static int GRID_SIZE = 16;
-	
+
 	public static int MOUNTAIN_UTILITY = 1000;
-	
-	//tower and missile constatns
+
+	// tower and missile constatns
 	public static int MISSILE_VELOCITY = 10;
 	public static int MAX_TOWER_RADIUS = 300;
 	public static int MIN_MISSILE_COOLDOWN = 30;
-	
+
 	public static int BASIC_MISSILE_ELF_DAMAGE = 10;
 	public static int BASIC_MISSILE_HUMAN_DAMAGE = 5;
 	public static int BASIC_MISSILE_DWARF_DAMAGE = 10;
 	public static int BASIC_MISSILE_HOBBIT_DAMAGE = 10;
-	
+
 	public static int BASIC_TOWER_COST = 250;
 	public static int BASIC_TOWER_RADIUS = 45;
 	public static int BASIC_TOWER_MAXCOOLDOWN = 45;
-	
+
 	public static int FAST_MISSILE_ELF_DAMAGE = 10;
 	public static int FAST_MISSILE_HUMAN_DAMAGE = 10;
 	public static int FAST_MISSILE_DWARF_DAMAGE = 10;
 	public static int FAST_MISSILE_HOBBIT_DAMAGE = 10;
-	
+
 	public static int FAST_TOWER_COST = 250;
 	public static int FAST_TOWER_RADIUS = 45;
 	public static int FAST_TOWER_MAXCOOLDOWN = 35;
-	
+
 	public static int LONG_DISTANCE_MISSILE_ELF_DAMAGE = 10;
 	public static int LONG_DISTANCE_MISSILE_HUMAN_DAMAGE = 10;
 	public static int LONG_DISTANCE_MISSILE_DWARF_DAMAGE = 10;
 	public static int LONG_DISTANCE_MISSILE_HOBBIT_DAMAGE = 10;
-	
+
 	public static int LONG_DISTANCE_TOWER_COST = 250;
 	public static int LONG_DISTANCE_TOWER_RADIUS = 45;
 	public static int LONG_DISTANCE_TOWER_MAXCOOLDOWN = 45;
-	
+
 	public static int GREAT_MISSILE_ELF_DAMAGE = 10;
 	public static int GREAT_MISSILE_HUMAN_DAMAGE = 10;
 	public static int GREAT_MISSILE_DWARF_DAMAGE = 10;
 	public static int GREAT_MISSILE_HOBBIT_DAMAGE = 10;
-	
+
 	public static int GREAT_TOWER_COST = 250;
 	public static int GREAT_TOWER_RADIUS = 45;
 	public static int GREAT_TOWER_MAXCOOLDOWN = 60;
-	
-	//enemy constants
+
+	// enemy constants
 	public static int ELF_HEALTH = 10;
 	public static int HUMAN_HEALTH = 10;
 	public static int DWARF_HEALTH = 10;
 	public static int HOBBIT_HEALTH = 10;
-	
-	
+
 	public static int ELF_DAMAGE = 10;
 	public static int HUMAN_DAMAGE = 10;
 	public static int DWARF_DAMAGE = 10;
 	public static int HOBBIT_DAMAGE = 10;
-	
+
 	public static int ELF_REWARD = 10;
 	public static int HUMAN_REWARD = 10;
 	public static int DWARF_REWARD = 10;
 	public static int HOBBIT_REWARD = 10;
-	
-	public static int ELF_COOLDOWN = 35;
-	public static int HUMAN_COOLDOWN = 3;
-	public static int DWARF_COOLDOWN = 35;
-	public static int HOBBIT_COOLDOWN = 35;
-	
+
+	public static int ELF_COOLDOWN = 13;
+	public static int HUMAN_COOLDOWN = 16;
+	public static int DWARF_COOLDOWN = 21;
+	public static int HOBBIT_COOLDOWN = 18;
+
 	public static int ARAGORN_HEALTH = 100;
 	public static int ARAGORN_DAMAGE = 10;
 	public static int ARAGORN_REWARD = 20;
-	
+
 	public static int GIMLI_HEALTH = 100;
 	public static int GIMLI_DAMAGE = 10;
 	public static int GIMLI_REWARD = 20;
-	
+
 	public static int LEGOLAS_HEALTH = 100;
 	public static int LEGOLAS_DAMAGE = 10;
 	public static int LEGOLAS_REWARD = 20;
-	
-	//trap & barricade constants
+
+	// trap & barricade constants
 	public static int DECREASE_DAMAGE_TRAP_COST = 250;
 	public static int DECREASE_DAMAGE_TRAP_WEAKEN_AMOUNT = 5;
-	
+
 	public static int SLOWER_TRAP__COST = 250;
 	public static int SLOWER_TRAP_SLOW_AMOUNT = 5;
-	
+
 	public static int POISONOUS_TRAP_COST = 250;
 	public static int POISONOUS_TRAP_POISON_AMOUNT = 5;
-	
+
 	public static int BARRICADE_COST = 50;
 	public static int BARRICADE_HEALTH = 50;
-	
-	//tower rune constants
+
+	// tower rune constants
 	public static int PLUS_DAMAGE_TOWER_RUNE_COST = 250;
 	public static int PLUS_DAMAGE_TOWER_RUNE_DAMAGE_AMOUNT = 5;
-	
+
 	public static int PLUS_RANGE_TOWER_RUNE_COST = 250;
 	public static int PLUS_RANGE_TOWER_RUNE_RANGE_AMOUNT = 5;
-	
+
 	public static int PLUS_SPEED_TOWER_RUNE_COST = 250;
 	public static int PLUS_SPEED_TOWER_RUNE_SPEED_AMOUNT = 5;
-	
+
 	public static int PLUS_ELF_DAMAGE_TOWER_RUNE_COST = 250;
 	public static int PLUS_ELF_DAMAGE_TOWER_RUNE_DAMAGE_AMOUNT = 5;
-	
+
 	public static int PLUS_DWARF_DAMAGE_TOWER_RUNE_COST = 250;
 	public static int PLUS_DWARF_DAMAGE_TOWER_RUNE_DAMAGE_AMOUNT = 5;
-	
+
 	public static int PLUS_HOBBIT_DAMAGE_TOWER_RUNE_COST = 250;
 	public static int PLUS_HOBBIT_DAMAGE_TOWER_RUNE_DAMAGE_AMOUNT = 5;
-	
-	//trap rune constants
+
+	// trap rune constants
 	public static int WEAKEN_TRAP_RUNE_COST = 250;
 	public static int WEAKEN_TRAP_RUNE_WEAKEN_AMOUNT = 5;
-	
+
 	public static int SLOWER_TRAP_RUNE_COST = 250;
 	public static int SLOWER_TRAP_RUNE_SLOW_AMOUNT = 5;
-	
+
 	public static int POISONOUS_TRAP_RUNE_COST = 250;
 	public static int POISONOUS_TRAP_RUNE_POISON_AMOUNT = 5;
-	
-	
-	//statusmodifier constants
+
+	// statusmodifier constants
 	public static int WEAKEN_STATUS_DURATION = 6;
 	public static int SLOWER_STATUS_DURATION = 6;
 	public static int POISONOUS_STATUS_DURATION = 6;
 	public static int POISONOUS_STATUS_TIME_INTERVAL = 6;
 
-	
-	
-//	public static int DAMAGE_BOOST_TOWER_RUNE = 3;
-	
-	//magic constatns
+	// public static int DAMAGE_BOOST_TOWER_RUNE = 3;
+
+	// magic constatns
 	public static int NAZGUL_MAGIC_COST = 500;
 	public static int NAZGUL_MAGIC_FREEZE_DURATION = 150;
-	
+
 	public static int ICE_WIND_MAGIC_COST = 500;
 	public static int ICE_WIND_MAGIC_DURATION = 150;
-	
+
 	public static int POISON_FOG_MAGIC_COST = 500;
 	public static int POISON_FOG_MAGIC_DURATION = 150;
-	
-	
+
 }
